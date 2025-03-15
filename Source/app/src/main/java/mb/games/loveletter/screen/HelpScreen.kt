4f80dee9b4fb3e1e79ff10e44014bf101c8ebe9e
MB <>
2025-03-15 16:22:45 +0100

    import images

--- Source/app/src/main/java/mb/games/loveletter/screen/HelpScreen.kt
+++ Source/app/src/main/java/mb/games/loveletter/screen/HelpScreen.kt
@@ -1,14 +1,21 @@
 package mb.games.loveletter.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import mb.games.loveletter.R
 
 @Composable
 fun HelpScreen(
@@ -21,15 +28,30 @@ fun HelpScreen(
 fun HelpView(
     onClick: () -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .fillMaxSize()
-            .clickable { onClick() },
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Text(text = "What do you need help with?")
+    Column {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .clickable { onClick() },
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(text = "What do you need help with?")
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .clickable { onClick() },
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.card_overview),
+                contentDescription = "card overview"
+            )
+        }
     }
+
 }
 
 @Preview(showBackground = true)
