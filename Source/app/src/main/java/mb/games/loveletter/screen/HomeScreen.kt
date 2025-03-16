f46521abc596cf0d409454268e2aaa5448d68586
MB <>
2025-03-16 16:57:14 +0100

    prettify homescreen

--- Source/app/src/main/java/mb/games/loveletter/screen/HomeScreen.kt
+++ Source/app/src/main/java/mb/games/loveletter/screen/HomeScreen.kt
@@ -1,7 +1,9 @@
 package mb.games.loveletter.screen
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -27,35 +29,44 @@ fun HomeScreen(
 fun MenuView(
     onNavigateToNewGame: () -> Unit, onNavigateToHelp: () -> Unit, onNavigateToAbout: () -> Unit
 ) {
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.Top,
-        horizontalArrangement = Arrangement.Center
+    Column(
+        modifier = Modifier.fillMaxSize()
     ) {
-        Text(
-            text = "Love Letter!",
-            fontFamily = FontFamily.SansSerif,
-            fontSize = TextUnit(value = 20F, TextUnitType.Sp)
-        )
-    }
-    LazyColumn {
-        items(homeMenuItems) { menuItem ->
-            MenuItemView(menuItem = menuItem, onClick = {
-                when (menuItem.name) {
-                    "New game" -> {
-                        onNavigateToNewGame()
-                    }
-
-                    "Help" -> {
-                        onNavigateToHelp()
-                    }
-
-                    else -> {
-                        onNavigateToAbout()
-                    }
-                }
-            })
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.Top,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = "Love Letter!",
+                fontFamily = FontFamily.SansSerif,
+                fontSize = TextUnit(value = 20F, TextUnitType.Sp)
+            )
         }
+
+        Row {
+            LazyColumn {
+                items(homeMenuItems) { menuItem ->
+                    MenuItemView(menuItem = menuItem, onClick = {
+                        when (menuItem.name) {
+                            "New game" -> {
+                                onNavigateToNewGame()
+                            }
+
+                            "Help" -> {
+                                onNavigateToHelp()
+                            }
+
+                            else -> {
+                                onNavigateToAbout()
+                            }
+                        }
+                    })
+                }
+            }
+
+        }
+
     }
 
 }
