a1ce04504d02f0c5bf64f9f36d9601d2c6e67d24
MB <>
2025-03-18 17:25:55 +0100

    fix error

--- Source/app/src/main/java/mb/games/loveletter/Navigation.kt
+++ Source/app/src/main/java/mb/games/loveletter/Navigation.kt
@@ -9,6 +9,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import mb.games.loveletter.screen.AboutScreen
 import mb.games.loveletter.screen.AddEditPlayerDetailView
+import mb.games.loveletter.screen.GameScreen
 import mb.games.loveletter.screen.HelpScreen
 import mb.games.loveletter.screen.HomeScreen
 import mb.games.loveletter.screen.NewGameScreen
@@ -17,8 +18,7 @@ import mb.games.loveletter.viewmodel.GameViewModel
 
 @Composable
 fun NavigationGraph(
-    navHostController: NavHostController,
-    gameViewModel: GameViewModel = viewModel()
+    navHostController: NavHostController, gameViewModel: GameViewModel = viewModel()
 ) {
     NavHost(navController = navHostController, startDestination = Screen.HomeScreen.route) {
         composable(Screen.HomeScreen.route) {
@@ -41,28 +41,26 @@ fun NavigationGraph(
             })
         }
         composable(Screen.NewGameScreen.route) {
-            NewGameScreen(
-                navController = navHostController,
+            NewGameScreen(navController = navHostController,
                 viewModel = gameViewModel,
                 onStartGame = {
                     navHostController.navigate(Screen.GameScreen.route)
                 },
                 onBackToHome = { navHostController.navigate(Screen.HomeScreen.route) })
         }
-        composable(Screen.AddPlayerScreen.route + "/{id}",
-            arguments = listOf(
-                navArgument("id") {
-                    type = NavType.IntType
-                    defaultValue = 0
-                    nullable = false
-                }
-            )
-        ) { entry ->
+        composable(Screen.GameScreen.route) {
+            GameScreen(navController = navHostController,
+                viewModel = gameViewModel,
+                onBackToHome = { navHostController.navigate(Screen.HomeScreen.route) })
+        }
+        composable(Screen.AddPlayerScreen.route + "/{id}", arguments = listOf(navArgument("id") {
+            type = NavType.IntType
+            defaultValue = 0
+            nullable = false
+        })) { entry ->
             val id = if (entry.arguments != null) entry.arguments!!.getInt("id") else 0
             AddEditPlayerDetailView(
-                id = id,
-                viewModel = gameViewModel,
-                navController = navHostController
+                id = id, viewModel = gameViewModel, navController = navHostController
             )
         }
     }
